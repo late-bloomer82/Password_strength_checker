8071b53c5a35ecfd5a554938c1444a661636bad2
late-bloomer82 <nateriver72@outlook.com>
2025-12-30 20:58:00 -0500

    changed logic. Removed validation and now instead evaluates password strength

--- main.py
+++ main.py
@@ -3,19 +3,30 @@ import re
 
 
 
-def validate_password(password):
+def check_password(password):
+    rating = 0
     if not isinstance(password, str):
         print("You must enter a string.")
     else:
-        if len(password)<9:
-            print("Password must have more 9 or more characters.")
-        if not re.search(r"[A-Z]",password):
-            print("Your password must have at least one uppercase letter.")
-        if not re.search(r"[a-z]", password):
-            print("Your password must have at least one lowercase letter.")
-        if not re.search(r"[0-9]", password):
-            print("Your password must have at least one number.")
-        if not re.search(r"[^a-zA-Z0-9]", password):
-            print("Your password must include at least one special character")
+        if len(password)>9:
+            rating += 3
+        if re.search(r"[A-Z]",password):
+            rating+= 1
+        if re.search(r"[a-z]", password):
+            rating+= 1
+        if re.search(r"[0-9]", password):
+            rating+= 2
+        if re.search(r"[^a-zA-Z0-9]", password):
+            rating+= 2
+        return rating
 
 
+
+def assess_password_strength(password):
+    rating = check_password(password)
+    if rating<=3:
+        print("Your password is weak.")
+    elif 4<= rating <=6:
+        print("Your password is medium.")
+    elif 7<= rating <=9:
+        print("Your password is strong.")
