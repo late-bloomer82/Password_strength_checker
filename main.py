343ac733566cc40038a852e4dc637e9f3df4c736
late-bloomer82 <nateriver72@outlook.com>
2025-12-30 22:14:03 -0500

    fixed wrong notation for accessing dictionary values in my function

--- main.py
+++ main.py
@@ -9,27 +9,26 @@ def analyze_password(password):
          "uppercase": bool(re.search(r"[A-Z]",password)),
          "lowercase":bool(re.search(r"[a-z]", password)),
          "numbers":bool(re.search(r"[0-9]", password)),
-         "special_character":re.search(r"[^a-zA-Z0-9]", password)
+         "special_character": bool(re.search(r"[^a-zA-Z0-9]", password))
    }
 
-def calculate_rating(password):
+def calculate_rating(password_obj):
     rating = 0
-    password_obj = analyze_password(password)
-    if password_obj.length:
+
+    if password_obj["length"]:
         rating+=3
-    if password_obj.uppercase:
+    if password_obj["uppercase"]:
         rating+=1
-    if password_obj.lowercase:
+    if password_obj["lowercase"]:
         rating+=1
-    if password_obj.numbers:
+    if password_obj["numbers"]:
         rating+=2
-    if password_obj.special_character:
+    if password_obj["special_character"]:
         rating+=2
     return rating
 
 
-def assess_password_strength(password):
-    rating = calculate_rating(password)
+def assess_password_strength(rating):
     if rating<=3:
         print("Your password is weak.")
     elif 4<= rating <=6:
@@ -38,19 +37,18 @@ def assess_password_strength(password):
         print("Your password is strong.")
 
 
-def suggest_improvements(password):
-    password_obj = analyze_password(password)
+def suggest_improvements(password_obj):
     answer = input("Would you like feedback on your password? (Y/N)")
     if answer.lower() == "y":
-        if not password_obj.length:
+        if not password_obj["length"]:
             print("Make your password length greater than 9.")
-        if not password_obj.uppercase:
+        if not password_obj["uppercase"]:
             print("Use at least one uppercase letter in your password.")
-        if not password_obj.lowercase:
+        if not password_obj["lowercase"]:
             print("Use at least one lowercase letter in your password.")
-        if not password_obj.numbers:
+        if not password_obj["numbers"]:
             print("Use at least one number in your password")
-        if not password_obj.special_character:
+        if not password_obj["special_character"]:
             print("Use at least one special character in your password")
     else:
         print("No problem!")
@@ -58,6 +56,10 @@ def suggest_improvements(password):
 
 
 
-
-
+# Test program
+password = getpass.getpass("Enter your password : ")
+password_obj = analyze_password(password)
+rating = calculate_rating(password_obj)
+assess_password_strength(rating)
+suggest_improvements(password_obj)
 
